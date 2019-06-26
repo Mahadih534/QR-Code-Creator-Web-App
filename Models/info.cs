8bb3601aa1d378fa8fb4dfc0db11fc500d328e6e
Md. Mahadi Hassan <mahadics@gmail.com>
2019-06-27 04:26:19 +0600

    v1

---
+++ Models/info.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace barCode.Models
+{
+   
+        public class info
+        {
+            public string Name { get; set; }
+            public string Phone { get; set; }
+            public string Email { get; set; }
+
+        }
+   
+}
